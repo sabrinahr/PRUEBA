400a8beacbb9de63c170c52a9f24315588988e67
sabrinahr <20223567@aloe.ulima.edu.pe>
2024-11-08 15:08:34 -0500

    final

--- JavaApplication14/src/javaapplication14/JavaApplication14.java
+++ JavaApplication14/src/javaapplication14/JavaApplication14.java
@@ -14,7 +14,7 @@ public class JavaApplication14 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("HolaAAAAAA");
     }
     
 }
