958441997cef50c0d48d6f1f6f7b77800598e75f
sabrinahr <20223567@aloe.ulima.edu.pe>
2024-11-08 14:52:02 -0500

    final

---
+++ JavaApplication14/src/javaapplication14/JavaApplication14.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication14;
+
+/**
+ *
+ * @author brina
+ */
+public class JavaApplication14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
